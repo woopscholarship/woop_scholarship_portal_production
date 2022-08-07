8a65f3bd04e8918eedb55b6eaa1db695ddd9dc88
Paul Bryan Reyes <75605942+paulreyesdigital@users.noreply.github.com>
2022-08-08 03:44:06 +0800

    removed outdated tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,11 @@
 const colors = require('tailwindcss/colors');
 
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
