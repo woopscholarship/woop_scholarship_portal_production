6fcc39a0b4e7dc837988f020c0c7fceab548f699
Paul Bryan Reyes <75605942+paulreyesdigital@users.noreply.github.com>
2022-08-10 04:50:33 +0800

    Create navigationItem.svelte

---
+++ src/components/sidebar/navigationItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let text: string;
+	export let href: string;
+
+	const activeSidebarStyle =
+		'bg-indigo-50 group flex items-center px-3 py-2 text-sm font-medium border-l-4"';
+	const defaultSidebarStyle =
+		'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-3 py-2 text-sm font-medium border-l-4';
+</script>
+
+<a
+	{href}
+	class={$page.url.pathname.includes(href) ? activeSidebarStyle : defaultSidebarStyle}
+	sveltekit:prefetch
+>
+	{text}
+</a>
