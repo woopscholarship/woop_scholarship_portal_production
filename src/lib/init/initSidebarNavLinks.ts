03680fa08dfd9e83ad8ff7ddf0e1c1e54eea4cac
Paul Bryan Reyes <75605942+paulreyesdigital@users.noreply.github.com>
2022-08-12 01:53:06 +0800

    setup routes for student and sponsor accounts

---
+++ src/lib/init/initSidebarNavLinks.ts
@@ -0,0 +1,33 @@
+export const adminRoutes = [
+  {
+    text: 'Review Students',
+    href: '/admin/review-students'
+  },
+  {
+    text: 'Review Sponsors',
+    href: '/admin/review-sponsors'
+  },
+  {
+    text: 'Review Scholarship Programs',
+    href: '/admin/review-scholarship-programs'
+  },
+  {
+    text: 'Review Grant Programs',
+    href: '/admin/review-grant-programs'
+  },
+  {
+    text: 'Review Student Applications',
+    href: '/admin/review-student-applications'
+  }
+];
+
+export const sponsorRoutes = [
+  {
+    text: 'Review Students',
+    href: '/sponsor/review-students'
+  },
+  {
+    text: 'Review Sponsors',
+    href: '/sponsor/review-sponsors'
+  },
+];
--- src/routes/__layout-dashboard.svelte
+++ src/routes/__layout-dashboard.svelte
@@ -1,40 +1,11 @@
 <script>
-	import { onMount } from 'svelte/internal';
+	import { adminRoutes } from '$lib/init/initSidebarNavLinks';
 	import { fade } from 'svelte/transition';
-	import { page } from '$app/stores';
 	import NavigationItem from '$root/components/sidebar/navigationItem.svelte';
-
-	const activeSidebarStyle =
-		'bg-indigo-50 group flex items-center px-3 py-2 text-sm font-medium border-l-4"';
-	const defaultSidebarStyle =
-		'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-3 py-2 text-sm font-medium border-l-4';
-
-	let navigationItems = [
-		{
-			text: 'Review Students',
-			href: '/admin/review-students'
-		},
-		{
-			text: 'Review Sponsors',
-			href: '/admin/review-sponsors'
-		},
-		{
-			text: 'Review Scholarship Programs',
-			href: '/admin/review-scholarship-programs'
-		},
-		{
-			text: 'Review Grant Programs',
-			href: '/admin/review-grant-programs'
-		},
-		{
-			text: 'Review Student Applications',
-			href: '/admin/review-student-applications'
-		}
-	];
 </script>
 
 <div transition:fade class="flex border w-screen min-h-screen">
-	<!-- This example requires Tailwind CSS v2.0+ -->
+	<!-- BRAND LOGO -->
 	<div class="flex flex-col flex-grow w-[20%] pt-5 pb-4 bg-white ">
 		<div class="flex justify-center flex-shrink-0 px-4 space-y-5">
 			<img
@@ -45,8 +16,9 @@
 			/>
 		</div>
 		<div class="mt-5 flex-grow flex flex-col">
+			<!-- NAVIGATION ELEMENT -->
 			<nav class="flex-1 pl-6 pr-6 bg-white space-y-1" aria-label="Sidebar">
-				{#each navigationItems as item}
+				{#each adminRoutes as item}
 					<NavigationItem text={item.text} href={item.href} />
 				{/each}
 			</nav>
